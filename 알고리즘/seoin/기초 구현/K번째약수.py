a181ad507d9bfcf048a3a5e14c7f2d3ab7dd1df7
inseo24 <84627144+inseo24@users.noreply.github.com>
2022-07-18 16:30:39 +0900

    :art: chore dir, add example py code

---
+++ 알고리즘/seoin/기초 구현/K번째약수.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+cnt = 0
+# k 번째 약수
+# N의 약수들 중 K번째로 작은 수
+# 존재하지 않을 경우 -1
+
+for i in range(1, n+1):
+    if n % i == 0:
+        cnt+=1
+    if cnt == k:
+        print(i)
+        break
+else:
+    print(-1)
