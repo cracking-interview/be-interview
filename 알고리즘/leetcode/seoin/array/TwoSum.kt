309c16ac5396ae680311f5ab057849a7c7463cc0
inseo24 <84627144+inseo24@users.noreply.github.com>
2022-08-01 23:28:54 +0900

    chore dir

---
+++ 알고리즘/leetcode/seoin/array/TwoSum.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val lens = nums.size
+        val ans : IntArray = intArrayOf(-1, -1)
+        for (i in 0 until lens) {
+            for (j in i + 1 until lens) {
+                if (nums[i] + nums[j] == target)  {
+                    ans[0] = i
+                    ans[1] = j
+                    break
+                }
+            }
+            if (ans[0] != -1) break
+        }
+        return ans
+    }
+}
